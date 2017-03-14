73000007f7106a7382a32b2a9bc4febf4c5faedf
Loona61 <aur6l@free.fr>
2017-03-14 21:24:50 +0100

    add API moviedb with popular film in homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Stormpath, Account } from 'angular-stormpath';
 import { Observable } from 'rxjs';
+import { MovieService } from './services/movie.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MovieService]
 })
 
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
 import { StormpathConfiguration, StormpathModule } from 'angular-stormpath';
 
 export function stormpathConfig(): StormpathConfiguration {
@@ -14,12 +15,14 @@ export function stormpathConfig(): StormpathConfiguration {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    JsonpModule,
 	StormpathModule
   ],
   providers: [{
