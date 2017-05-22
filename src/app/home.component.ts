5bfb5cba67359f3a0c2ae72107dfc999cf49190e
Loona61 <aur6l@free.fr>
2017-05-22 17:19:01 +0200

    fix login problem

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -4,7 +4,7 @@ import { Auth } from './services/auth.service';
 @Component({
   selector: 'home',
   template: `
-    <h4 *ngIf="auth.authenticated()">You are logged in {{auth.userProfile.name}} </h4>
+    <h4 *ngIf="auth.authenticated() && auth.userProfile">You are logged in {{auth.userProfile.nickname}} </h4>
     <h4 *ngIf="!auth.authenticated()">You are not logged in, please click 'Log in' button to login</h4>
   `
 })
