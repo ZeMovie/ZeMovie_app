b15679653590d009b3ab247390b50b811c6df5a0
Loona61 <aur6l@free.fr>
2017-05-22 18:11:20 +0200

    Revert "Merge pull request #2 from ZeMovie/clean
    This reverts commit a8afb52b31ab5893bfb5927ff5ec85bc90f5cf1c, reversing
    changes made to 9ea77a1ab57a1a8b4c894f2729c4baa56a0f0b74.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,40 @@ import { Component } from '@angular/core';
 import { Stormpath, Account } from 'angular-stormpath';
 import { Observable } from 'rxjs';
 import { MovieService } from './services/movie.service';
-import { Auth } from './services/auth.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [MovieService,Auth]
+  providers: [MovieService]
 })
 
 export class AppComponent {
-  constructor(private auth: Auth) {}
+  private user$: Observable<Account | boolean>;
+  private loggedIn$: Observable<boolean>;
+  private login: boolean;
+  private register: boolean;
+ 
+  constructor(public stormpath: Stormpath) {
+  }
+ 
+  ngOnInit() {
+    this.login = true;
+    this.register = false;
+    this.user$ = this.stormpath.user$;
+    this.loggedIn$ = this.user$.map(user => !!user);
+  }
+ 
+  showLogin() {
+    this.login = !(this.register = false);
+  }
+ 
+  showRegister() {
+    this.register = !(this.login = false);
+  }
+ 
+  logout() {
+    this.stormpath.logout();
+  }
 }
