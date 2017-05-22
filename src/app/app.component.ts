9e8df23d2123e9fd83947e597b8072b9e6d244d9
Loona61 <aur6l@free.fr>
2017-05-22 19:25:15 +0200

    new project with auth0 for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,15 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MovieService } from './services/movie.service';
+import { Auth } from './services/auth.service';
 
 @Component({
-  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [MovieService]
+  providers: [MovieService,Auth]
 })
 
 export class AppComponent {
-  private user$: Observable<Account | boolean>;
-  private loggedIn$: Observable<boolean>;
-  private login: boolean;
-  private register: boolean;
- 
-  constructor(public stormpath: Stormpath) {
-  }
- 
-  ngOnInit() {
-    this.login = true;
-    this.register = false;
-    this.user$ = this.stormpath.user$;
-    this.loggedIn$ = this.user$.map(user => !!user);
-  }
- 
-  showLogin() {
-    this.login = !(this.register = false);
-  }
- 
-  showRegister() {
-    this.register = !(this.login = false);
-  }
- 
-  logout() {
-    this.stormpath.logout();
-  }
+  constructor(private auth: Auth) {}
 }
