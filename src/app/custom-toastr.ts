6937cc6e2eb74cf4d50510d1a5786c798c922a6c
Loona61 <aur6l@free.fr>
2017-05-31 16:30:15 +0200

    some css (add fontawesome module)

--- src/app/custom-toastr.ts
+++ src/app/custom-toastr.ts
@@ -1,7 +1,7 @@
 import {ToastOptions} from 'ng2-toastr';
 
 export class CustomOption extends ToastOptions {
-  animate = 'fade'; // you can override any options available
+  animate = 'flyRight'; // you can override any options available
   newestOnTop = false;
   showCloseButton = true;
   positionClass = "toast-top-center";
